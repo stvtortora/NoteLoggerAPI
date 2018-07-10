75f829ff58534f418ffbc8c113d340a80f8c5782
Steven <stvtortora@gmail.com>
2018-07-10 19:09:04 -0400

    add GET /notes/:id route with tests

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectId} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Note} = require('./models/note');
@@ -26,6 +27,23 @@ app.get('/notes', (req, res) => {
     res.send({notes});
   }, (e) =>{
     res.status(400).send(e);
+  });
+});
+
+app.get('/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Note.findById(id).then(note => {
+    if(!note){
+      return res.status(404).send();res.send({note})
+    }
+    res.send({note})
+  }).catch((e) => {
+    res.status(400).send();
   });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Note} = require('./../models/note');
 
 const notes = [{
+  _id: new ObjectId(),
   text: 'test note 1'
-},{
+}, {
+  _id: new ObjectId(),
   text: 'test note 2'
 }];
 
@@ -69,3 +72,30 @@ describe('GET /notes', () => {
     .end(done)
   });
 });
+
+describe('GET /notes/:id', () => {
+  it('should get a single note', (done) => {
+    request(app)
+    .get(`/notes/${notes[0]._id.toHexString()}`)
+    .expect(200)
+    .expect(res => {
+      expect(res.body.note.text).toBe(notes[0].text)
+    })
+    .end(done);
+  });
+
+  it('should return 404 if note is not found', (done) => {
+    const outSideId = new ObjectId().toHexString();
+    request(app)
+    .get(`/notes/${outSideId}`)
+    .expect(404)
+    .end(done)
+  });
+
+  it('should return 404 for invalid ids', (done) => {
+    request(app)
+    .get(`/notes/invalidId`)
+    .expect(404)
+    .end(done)
+  })
+})
