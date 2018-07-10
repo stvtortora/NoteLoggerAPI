cf27033f0a7bdd0150125c828e34deaead1c06f6
Steven <stvtortora@gmail.com>
2018-07-10 18:06:55 -0400

    create tests for GET /notes

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Note} = require('./../models/note');
 
+const notes = [{
+  text: 'test note 1'
+},{
+  text: 'test note 2'
+}];
+
 beforeEach((done) => {
   Note.remove({}).then(() => {
-    done();
-  });
+    return Note.insertMany(notes);
+  }).then(() => done());
 });
 
 describe('POST /notes', () => {
@@ -26,7 +32,7 @@ describe('POST /notes', () => {
         return done(err);
       }
 
-      Note.find().then((notes) => {
+      Note.find({text}).then((notes) => {
         expect(notes.length).toBe(1);
         expect(notes[0].text).toBe(text);
         done();
@@ -45,9 +51,21 @@ describe('POST /notes', () => {
       }
 
       Note.find().then((notes) => {
-        expect(notes.length).toBe(0);
+        expect(notes.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
+  });
+});
+
+describe('GET /notes', () => {
+  it('should get all notes', (done) => {
+    request(app)
+    .get('/notes')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.notes.length).toBe(2);
+    })
+    .end(done)
   });
 });
