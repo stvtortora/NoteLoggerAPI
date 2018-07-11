b837cd9a7a55e50f9115f6de39d028aa3a954553
Steven <stvtortora@gmail.com>
2018-07-11 18:22:23 -0400

    create test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/app');
+mongoose.connect(process.env.MONGO_URI) || 'mongodb://localhost:27017/noteApp';
 
 module.exports = {mongoose};
