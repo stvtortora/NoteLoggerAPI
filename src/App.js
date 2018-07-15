fac35e73767f4a2b05b44c508b46961de867c3d7
Steven <stvtortora@gmail.com>
2018-07-14 22:12:08 -0400

    write App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Content from './components/content/content';
+import Header from './components/header';
+import Footer from './components/footer';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Content />
+        <Footer />
       </div>
     );
   }
