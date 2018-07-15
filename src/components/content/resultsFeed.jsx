4dd20ba76793592a6dcc473e0e5da13ca8834ccf
Steven <stvtortora@gmail.com>
2018-07-15 00:06:12 -0400

    pass <ResultsFeed> tests

--- src/components/content/resultsFeed.jsx
+++ src/components/content/resultsFeed.jsx
@@ -1,14 +1,59 @@
 import React from 'react'
+import Post from './post';
 
 
 class ResultsFeed extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      postsData: this.props.searchResults,
+      startIdx: 0,
+      endIdx: 3
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.searchResults !== prevProps.searchResults) {
+      this.setState({
+        postsData: this.props.searchResults,
+        startIdx: 0,
+        endIdx: 3
+      });
+    }
+  }
+
+  handleScroll(offSet) {
+    return () => {
+      this.setState({
+        startIdx: this.state.startIdx + offSet,
+        endIdx: this.state.endIdx + offSet
+      })
+    }
+  }
 
   render() {
+    let postsData = this.state.postsData;
+
+    if(postsData.length){
+      const posts = postsData.slice(this.state.startIdx, this.state.endIdx).map((postData, i) => {
+        return <Post key={i} data={postData} />
+      });
+      const previousArrow = this.state.startIdx > 0 ? <i className='previous' onClick={ this.handleScroll(-3) }/ > : null;
+      const nextArrow = this.state.endIdx < postsData.length ? <i className='next' onClick={ this.handleScroll(3) }/ > : null;
 
       return(
         <div className='feed'>
+          <div>{previousArrow}</div>
+          <div>
+            {posts}
+          </div>
+          <div>{nextArrow}</div>
         </div>
       )
+    }
+
+    return null;
   }
 }
 
