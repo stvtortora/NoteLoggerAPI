1e263c1d02e7f0af6f8418c3ed3b76dcc1b0bc81
Steven <stvtortora@gmail.com>
2018-07-19 01:43:57 -0400

    add cors middleware

--- server/server.js
+++ server/server.js
@@ -14,7 +14,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/subreddits', authenticate, (req, res) => {
   const subreddit = new SubReddit({
