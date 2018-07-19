90c92c9619782cbe778b9c6f648b5ffeda947b15
Steven <stvtortora@gmail.com>
2018-07-19 00:26:29 -0400

    delete commented out code

--- server/config/config.js
+++ server/config/config.js
@@ -9,11 +9,3 @@ if (env === 'development' || env ==='test') {
     process.env[key] = envConfig[key];
   })
 }
-
-// if(env === 'development'){
-//   process.env.PORT = 3000;
-//   process.env.MONGO_URI = 'mongodb://localhost:27017/subredditapp';
-// }else if (env === 'test'){
-//   process.env.PORT = 3000;
-//   process.env.MONGO_URI = 'mongodb://localhost:27017/subredditapptest';
-// }
