1a25e408c96fd679907548e9f1c3b8787179c3e7
Steven <stvtortora@gmail.com>
2018-07-19 15:27:53 -0400

    add x-auth to accepted res headers

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "X-Auth, Content-Type, Accept");
   next();
 });
 
