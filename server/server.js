c9de13c3fe8ab8f75b5759fb9df6ebabb4c272f0
Steven <stvtortora@gmail.com>
2018-07-22 23:37:40 -0400

    revise headers

--- server/server.js
+++ server/server.js
@@ -17,8 +17,7 @@ app.use(bodyParser.json());
 // app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type", "Origin, X-Requested-With, X-Content-Type, Accept");
-  res.header("Access-Control-Expose-Headers", "x-auth");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
