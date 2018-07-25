3d434607ed51db6e6df344223c18924d34560c36
Steven <stvtortora@gmail.com>
2018-07-25 16:41:11 -0400

    add cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH', 'OPTIONS');
