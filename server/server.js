648e523dde2aa3b40f70880876b423f50941e28b
Steven <stvtortora@gmail.com>
2018-07-25 17:13:23 -0400

    revise headers

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH', 'OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, 'x-auth', Accept");
   res.header("Access-Control-Expose-Headers", "x-auth")
   next();
