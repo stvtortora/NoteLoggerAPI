dad8533b239594c9672a2bc01067641d1d26799f
Steven <stvtortora@gmail.com>
2018-08-07 14:47:23 -0400

    modify static build folder

--- server.js
+++ server.js
@@ -137,11 +137,11 @@ app.delete('/api/users/current/token', authenticate, (req, res) => {
 })
 
 // if (process.env.NODE_EV === 'production') {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-
-  app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  })
+  // app.use(express.static(path.join(__dirname, "client", "build")));
+  //
+  // app.get('*', (req, res) => {
+  //    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // })
 // }
 
 
